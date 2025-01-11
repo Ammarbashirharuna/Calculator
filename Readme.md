ceef077d03d553548340039424b9a19358d16337
ammarbashirharuna <ammarbashirharuna2020@gmail.com>
2025-01-11 22:16:41 +0100

    adding readme

---
+++ Readme.md
@@ -0,0 +1,3 @@
+## Calculator
+This is my calculator that am working on using oop...
+more update ahead.🙌
--- src/Operation.java
+++ src/Operation.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Operation {
+    public static double calc(double firstNum, String operator, double secondNum){
+        return switch (operator) {
+            case "+" -> firstNum + secondNum;
+            case "-" -> firstNum - secondNum;
+            case "*" -> firstNum * secondNum;
+            case "%" -> firstNum % secondNum;
+            case "/" -> firstNum / secondNum;
+            default -> {
+                System.out.println("The operator you enter is invalid ");
+                yield 0;
+            }
+        };
+    }
 }
